5eacce4711d490a54373c7802424e6de95806fee
Ivey Davis <idavis@caltech.edu>
2024-06-17 14:00:18 -0700

    more examples

--- flare_id_and_classification_example.py
+++ flare_id_and_classification_example.py
@@ -6,15 +6,18 @@ Created on Mon Jul  3 14:31:30 2023
 @author: idavis
 """
 
-from FlareFlaggingClass import *
+from flare_id import *
 import numpy as np
 import lightkurve as lk
 from astropy import units as un
 
 #%% Starting from scratch with bare minimum information on a star:
+tic_num = 416741712
+radius = 0.91 # solar radii
+temperature = 5665 # Kelvin
 
 # make Star object:
-star = Star(tic_num=416741712, radius = 0.91, temperature=5665)
+star = Star(tic_num=tic_num, radius=radius, temperature=temperature)
 
 print(f'Calculated period: {star.period}')
 print(f'Sectors the star appears in: {star.lcs.sector}')
@@ -63,18 +66,18 @@ flares.PlotLightCurves()
 print(f'Flare rate after flagging: {flares.flare_rate}')
 print(flares.flare_table)
 
-# write out data:
-flares.WriteOutData('Flare_Flagging_Example/')
+# write out data; default is to save to the directory you're working in:
+flares.WriteOutData()
 
 #%% Starting from already having a light curve collection + star information:
     
 # get some example data and use a different integration time:
-lcs = lk.search_lightcurve('TIC 416741712', author = 'SPOC',exptime = 120).download_all()
+lcs = lk.search_lightcurve(f'TIC {tic_num}', author = 'SPOC',exptime = 120).download_all()
 for i in range(len(lcs)):
     lc = lcs[i].bin(5 * un.min)
     lcs[i] = lc
 
-star = Star(tic_num=416741712, radius = 0.91, temperature=5665,lcs = lcs)
+star = Star(tic_num=tic_num, radius=radius, temperature=temperature, lcs=lcs)
 flares = Flares(star, int_time=300 * un.s)
 flares.FlagLightCurves()
 flares.FindAllFlares(n_points_max=2)
